b9fd161d2056b01475d7428a09ec15b989841b05
Code Lao <109976097+codelao@users.noreply.github.com>
2022-12-03 20:04:41 +0300

    1.1

--- code/markups.py
+++ code/markups.py
@@ -1,11 +1,12 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
-btnProfile = KeyboardButton('Профиль')
+
+btnProfile = KeyboardButton('Profile')
 profileKeyboard = ReplyKeyboardMarkup(resize_keyboard = True).add(btnProfile)
 
-#subscribe buttons
-btnUrl = InlineKeyboardButton(text='Подписаться', url='https://t.me/...')
-btnsubdone = InlineKeyboardButton(text='Проверить подписку', callback_data='subdone')
+
+btnUrl = InlineKeyboardButton(text='Subscribe', url='https://t.me/codelao1')
+btnsubdone = InlineKeyboardButton(text='Check subscription', callback_data='subdone')
 checkSubMenu = InlineKeyboardMarkup(row_width=1)
 checkSubMenu.insert(btnUrl)
 checkSubMenu.insert(btnsubdone)
